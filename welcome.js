69a94b4de46a5fd2369d4b75981406412ab6e7d3
Kenny2github <kenny2minecraft@gmail.com>
2017-05-10 21:40:29 +0800

    Made file

---
+++ welcome.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Wiki Welcome
+// @namespace    http://Kenny2github.github.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://wiki.scratch.mit.edu/w/index.php?title=User_talk:*&action=edit&section=new
+// @grant        none
+// ==/UserScript==
+
+var welcomeMessage = '{{User:Kenny2scratch/Welcome}}'; //Replace this with your welcome message
+function elem(id) {
+    return document.getElementById(id);
+}
+elem('wpSummary').onblur = function(){
+    if (elem('wpSummary').value === 'Welcome!') {
+        elem('wpTextbox1').value = welcomeMessage + '~~~~';
+        elem('wpSave').click();
+    }
+};
