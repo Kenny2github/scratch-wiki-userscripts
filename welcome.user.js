ba9802509dc65d3c849d7ca1f87f4160ddd16b07
Ken <kenny2minecraft@gmail.com>
2018-07-14 09:39:06 +0200

    Update welcome.user.js

--- welcome.user.js
+++ welcome.user.js
@@ -1,20 +1,26 @@
 // ==UserScript==
 // @name         Wiki Welcome
 // @namespace    http://github.com/Kenny2github
-// @version      0.3
+// @version      0.4
 // @description  Give new users some love!
 // @author       Kenny2github
 // @updateURL    https://github.com/Kenny2github/scratch-wiki-userscripts/raw/master/welcome.user.js
 // @match        https://en.scratch-wiki.info/w/index.php?title=User_talk:*&action=edit&section=new
 // ==/UserScript==
 
-var welcomeMessage = '{{User:' + document.querySelector('span.profile-name').innerHTML + '/Welcome}}';
 function elem(id) {
     return document.getElementById(id);
 }
-elem('wpSummary').onblur = function(){
-    if (elem('wpSummary').value === 'Welcome!') {
-        elem('wpTextbox1').value = welcomeMessage + '~~~~';
+function mouse(e) {
+    e.preventDefault();
+    mw.toolbar.insertTags('{{User:' + mw.user.getName() + '/Welcome', '}}~~~~', '');
+    setTimeout(function(){
+        window.removeEventListener('click', mouse); //remove first or the wpSave click is handled too
         elem('wpSave').click();
+    }, 50);
+}
+elem('wpSummary').onblur = function(e){
+    if (elem('wpSummary').value === 'Welcome!') {
+        window.addEventListener('click', mouse);
     }
 };
