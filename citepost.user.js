da5a47c76a909a2b7d1ff40f00fb0e1ce4b7818d
Kenny2github <kenny2minecraft@gmail.com>
2021-06-06 23:08:42 +0800

    Fix only post ID being copied, not internal link
    Also fix egregious style errors, bump version, update description, and make use of template literals.

--- citepost.user.js
+++ citepost.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Cite This Post
 // @namespace    https://github.com/Kenny2github
-// @version      0.4
-// @description  get a reference in the correct format copied in just a couple clicks!
+// @version      0.5
+// @description  Get a forum archive reference in the correct format copied in just a couple clicks!
 // @author       Kenny2github
 // @updateURL    https://github.com/Kenny2github/scratch-wiki-userscripts/raw/master/citepost.user.js
 // @match        https://scratcharchive.asun.co/forums/viewtopic.php*
@@ -11,30 +11,32 @@
 
 if (window.location.hostname == 'scratcharchive.asun.co') {
     document.querySelectorAll('div.blockpost').forEach(function(i){
-        cite = document.createElement('a');
+        var cite = document.createElement('a');
         cite.href = '#';
         cite.innerHTML = 'Cite this post&nbsp;';
         cite.style.float = 'right';
         cite.onclick = function(){
-            id = i.id; //id of div
-            username = document.querySelector('#' + id + ' > div > div > div.postleft > dl > dt > strong > a').innerHTML; //username of poster
-            datem = this.previous().innerHTML.match(/(\d+)-(\d+)-(\d+)/); //match date
-            date = datem[3].replace(/^0*/,'') + '/' + datem[2].replace(/^0*/,'') + '/' + datem[1].replace(/^0*/, ''); //piece date together from match
-            sel = window.getSelection() + ''; //cast selection to string
+            var id = i.id; //id of div
+            var username = document.querySelector(`#${id} > div > div > div.postleft > dl > dt > strong > a`).innerHTML; //username of poster
+            var datem = this.previous().innerHTML.match(/(\d+)-(\d+)-(\d+)/); //match date
+            var date = datem[3].replace(/^0*/,'') + '/' + datem[2].replace(/^0*/,'') + '/' + datem[1].replace(/^0*/, ''); //piece date together from match
+            var sel = window.getSelection() + ''; //cast selection to string
+            var defquote;
             if (i.classList.contains('firstpost')) {
-                defquote = document.querySelector('#' + id + ' > div > div > div.postright > h3').textContent + '[title]';
+                defquote = document.querySelector(`#${id} > div > div > div.postright > h3`).textContent + '[title]';
             } else {
-                defquote = document.querySelector('#' + id + ' > div > div > div.postright > div.postmsg').children[0].innerText.split('\n')[0];
+                defquote = document.querySelector(`#${id} > div > div > div.postright > div.postmsg`).children[0].innerText.split('\n')[0];
             }
-            quote = sel ? sel : defquote; //selection or title
-            tid = window.location.href.split('?', 2)[1].match(/id=(\d+)/)[1];
-            post = i.classList.contains('firstpost') ? '[[ar-topic:' + tid + ']]' : id.substr(1);
-            ref = '{{cite post|' + username + '|' + date + '|' + quote + '|' + post + '}}'; //piece reference together
+            var quote = sel ? sel : defquote; //selection or title
+            var tid = window.location.href.split('?', 2)[1].match(/id=(\d+)/)[1];
+            var post = i.classList.contains('firstpost') ? `[[ar-topic:${tid}]]` : `[[ar-post:${id.substr(1)}]]`;
+            var ref = `{{cite post|${username}|${date}|${quote}|${post}}}`; //piece reference together
             //copy reference to clipboard
-            temp = document.createElement('textarea');
+            var temp = document.createElement('textarea');
             temp.textContent = ref;
             document.body.appendChild(temp);
             temp.select();
+            var ret;
             try {
                 document.execCommand('copy');
                 ret = 'Copied.';
